63f526965c9681182f96df7b68339fba777e477e
Robert Hallsey <rhallsey@yahoo.com>
2015-05-28 11:49:34 -0700

    Reset

--- include.php
+++
@@ -1,7 +0,0 @@
-<?php
-  define ('RH_BASE_URL', 'http://localhost/survey/');
-  define ('RH_BASE_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR);
-  define ('RH_SURVEY_PAGE_URL', RH_BASE_URL . 'survey.php');
-  define ('RH_CONFIRM_PAGE_URL', RH_BASE_URL . 'confirm.php');
-  define ('EOL', "\r\n");
-?>
