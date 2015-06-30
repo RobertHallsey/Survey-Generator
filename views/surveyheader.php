ea52f19a9c687f76691efe3a08571be1eceed5cd
Robert Hallsey <rhallsey@yahoo.com>
2015-06-30 09:41:11 -0700

    Edits

--- views/surveyheader.php
+++ views/surveyheader.php
@@ -1,10 +1,9 @@
 <?php
 
 /**
- * The Survey Plugin for Wolf CMS makes it easy to conduct custom surveys.
+ * The Survey Generator makes it easy to conduct custom surveys.
  *
- * This file is part of the Survey Plugin for Wolf CMS.
- *
+ * This file is part of the Survey Generator.
  * @author Robert Hallsey <rhallsey@yahoo.com>
  * @copyright Robert Hallsey, 2015
  * @license http://www.gnu.org/licenses/gpl.html GPLv3 license
